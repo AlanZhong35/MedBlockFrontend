048e9dac0bf1581bb7b515eb1c3fdfee32559557
AlanZhong35 <58539789+AlanZhong35@users.noreply.github.com>
2023-11-11 19:47:02 -0600

    Files

---
+++ App.svelte
@@ -0,0 +1,32 @@
+<nav>
+	<a href="/">home</a>
+	<a href="/about">upload</a>
+</nav>
+
+<main>
+	<h1>Welcome to Med-BlockChain</h1>
+	<p>Visit the <a href="https://ayushksingh.com/">CEO's website </a> .</p>
+	<p>The next generation of personal data storage is here...</p>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
