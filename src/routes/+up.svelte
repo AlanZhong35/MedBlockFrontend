2dd2fec9f09fc616cb2fd0dcc4b18646ea8dd643
AlanZhong35 <alantrex5@gmail.com>
2023-11-11 18:28:56 -0600

    frontend commit

---
+++ src/routes/+up.svelte
@@ -0,0 +1,33 @@
+<script>
+	import Button from "./Button.svelte"
+</script>
+
+
+<main>
+	<h1>Upload here</h1>
+	<Button class="danger lg" on:click={()=>console.log("Clicked")}>
+        My Button
+    </Button>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
